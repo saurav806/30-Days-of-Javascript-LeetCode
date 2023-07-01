cd245394570228be052dfea2439b4a1a462b3dc9
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-07-01 23:50:54 +0530

    Counter II

---
+++ 2665. Counter II.js
@@ -0,0 +1,25 @@
+//Link:- https://leetcode.com/problems/counter-ii/?envType=study-plan-v2&envId=30-days-of-javascript
+
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let num=init;
+    return {
+        increment: ()=> 
+            ++num,
+        decrement: ()=> 
+            --num,
+        reset: ()=>  
+            num=init
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
