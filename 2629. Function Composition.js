40a1cfa630c6f4f65e8aeeee9b0f4da5d2ffe9b5
Saurav Kumar <54290238+saurav806@users.noreply.github.com>
2023-07-05 23:42:13 +0530

    Day 5

---
+++ 2629. Function Composition.js
@@ -0,0 +1,14 @@
+//Link:- https://leetcode.com/problems/function-composition/description/
+
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return x => functions.reduceRight((acc,f) => f(acc),x)
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
